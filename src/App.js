9695aa48dadfc304ca99187e356942547ca05207
milan.zivanov <milan.zivanov@gmail.com>
2021-08-12 14:22:46 +0200

    Passing props to the component, Include multiple components and manage the state, Adding a click handler, Using hooks to update the component state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { useState } from "react";
 import './App.css';
 import Button from './components/Button';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = (increment) => {
+    setCount(count + increment);
+  }
+
   return (
     <div className="App">
-      <Button></Button>
+      <Button 
+        increment={1}
+        onClickFunction={incrementCount}>
+      </Button>
+      <Button 
+        increment={10}
+        onClickFunction={incrementCount}>
+      </Button>
+      <Button 
+        increment={100}
+        onClickFunction={incrementCount}>
+      </Button>
+      <Button 
+        increment={1000}
+        onClickFunction={incrementCount}>
+      </Button>
+
+      <span>{count}</span>
     </div>
   );
 }
